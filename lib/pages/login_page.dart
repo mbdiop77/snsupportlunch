cfce3216a8d871fa03d80292d21c1d73a0a12d78
Mame Bamba Diop <mamebambadiop5@gmal.com>
2026-04-06 04:42:50 +0200

    init

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -100,7 +100,7 @@ class _LoginPageState extends State<LoginPage> {
       if (mounted) setState(() => isLoading = true);
 
       await authService.signInWithGoogle(
-        redirectTo: 'https://e-support-lunch.com/', // 🔥 URL locale
+        redirectTo: 'https://e-support-lunch.com', // 🔥 URL locale
       );
 
       // ⚠️ Le listener gère tout
